1ab6caf8e6c74a0121da5f7818b609f85326f3f2
Ali-RTFM <alialibadaoui@gmail.com>
2020-10-04 19:00:29 +0200

    Mise en Place de la selection et des details des films

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 
 export default class MovieElement extends Component {
+  mouseEnter = () => {
+    this.props.updateSelectedMovie(this.props.movie.title);
+  };
+
   render() {
     return (
-      <div className="w-25 p-2">
-        <div className="card">
-          <img
-            src="https://www.zerodeconduite.net/sites/default/files/styles/large_film/public/film/affiche_gladiator.png?itok=ZFq8B_jW"
-            alt="film"
-            className="card-img-top"
-          />
-          <h5 className="card-title">Gladiator</h5>
-          <p className="card-text">
-            Le général romain Maximus est le plus fidèle soutien de l’empereur Marc Aurèle, qu’il a conduit de victoire en victoire avec une bravoure
-            et un dévouement exemplaires.
-          </p>
+      <div onMouseEnter={this.mouseEnter} className="w-50 p-2">
+        <div className="border d-flex ">
+          <img width="150" height="200" src={this.props.movie.img} alt="film" />
+          <div className="flex-fill d-flex flex-column p-3">
+            <h5>{this.props.movie.title}</h5>
+            <hr className="w-100" />
+            <p>{this.props.movie.details}</p>
+          </div>
         </div>
       </div>
     );
